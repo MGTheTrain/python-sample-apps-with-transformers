426a4ce698b93c92a0433f13744212f3c10d4f13
Marvin Gajek <Marvin.Gajek1@web.de>
2023-11-04 17:10:30 +0100

    update README and sample app

--- samples/chatbot.py
+++ samples/chatbot.py
@@ -1,24 +1,29 @@
+import argparse
 import tensorflow as tf
 from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
 
-# Load pre-trained GPT-2 model and tokenizer
-# model_name = "gpt2" # medium transformer model - less accuracy - requires less hardware resources
-model_name = "gpt2-large" # large transformer model - more accuracy - requires more hardware resources
-# model_name = "gpt2-x1" # very large transformer model - even more accuracy - requires even more hardware resources
-model = TFGPT2LMHeadModel.from_pretrained(model_name)
-tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+def main(args):
+    # Load the pre-trained GPT-2 model and tokenizer
+    model = TFGPT2LMHeadModel.from_pretrained(args.model_name)
+    tokenizer = GPT2Tokenizer.from_pretrained(args.model_name)
 
-# Chatbot loop
-while True:
-    user_input = input("You: ")
-    if user_input.lower() == "exit":
-        break
+    # Chatbot loop
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "exit":
+            break
 
-    # Tokenize the user input
-    input_ids = tokenizer.encode("You: " + user_input, return_tensors="tf")
+        # Tokenize the user input
+        input_ids = tokenizer.encode("You: " + user_input, return_tensors="tf")
 
-    # Generate a response from the model
-    response = model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=50256)
-    chatbot_output = tokenizer.decode(response[0], skip_special_tokens=True)
+        # Generate a response from the model
+        response = model.generate(input_ids, max_length=100, num_return_sequences=1, pad_token_id=50256)
+        chatbot_output = tokenizer.decode(response[0], skip_special_tokens=True)
 
-    print("Chatbot:", chatbot_output)
+        print("Chatbot:", chatbot_output)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Simple GPT-2 Chatbot")
+    parser.add_argument("--model_name", type=str, default="gpt2", help="Pre-trained model name (e.g., gpt2, gpt2-medium, gpt2-large, etc.)")
+    args = parser.parse_args()
+    main(args)
